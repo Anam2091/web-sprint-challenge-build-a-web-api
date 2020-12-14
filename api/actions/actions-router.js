d936d72e38f2b0833186208cbc15f0ac395fc6db
Anam2091 <56008677+Anam2091@users.noreply.github.com>
2020-12-13 22:38:15 -0700

    actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,17 @@
+server.get('/api/actions',(req,res)=>{
+    res.status(200).send([])
+
+})
+
+server.get('/api/actions/:id',(req,res)=>{
+    const id = req.body.id;
+    res.status(200).send({id})
+})
+
+server.post('/api/actions',(req,res)=>{
+    const action= req.action
+    res.status(200).send(action)
+})
+
+
 // Write your "actions" router here!
